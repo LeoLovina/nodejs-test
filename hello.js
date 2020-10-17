51b8ca86fcc664cb7c6fcea6fd90ed99d87bef90
Leo Chen <leo.lovina@gmail.com>
2020-10-17 15:15:41 +0800

    Create hello.js
    init

---
+++ hello.js
@@ -0,0 +1,17 @@
+// var message = 'Hello World';
+// console.log(message);
+
+var http = require("http");
+
+//create HTTP server and listen to port 8000
+http.createServer(function (request, response) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body "Hello World"
+   response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8000/')
